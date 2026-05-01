74d654bae955e6d65fe2a6e20b2f1ef8bf439b15
Ubuntu <dinhhung2772005@gmail.com>
2026-05-01 14:37:42 +0700

    Added authentication

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,10 +6,12 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
-router.register('user-profiles', views.UserProfilesViewSet, basename='user-profiles')
+router.register('user-profiles', views.UserProfilesViewSet)
+router.register('profile-feed', views.UserProfileFeedViewSet)
 
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/',views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
